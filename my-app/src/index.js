b582ca0c8bf3cfb7df8731280d1f641e442e4b34
Bovvan <shamkov2323@gmail.com>
2023-12-23 16:39:27 +0200

    Trying to deploy attempt 2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,6 +11,12 @@ root.render(
   </React.StrictMode>
 );
 
+app.use(express.static(__dirname));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
